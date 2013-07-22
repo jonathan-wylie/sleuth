47f3697215363b7ed32627ad5c91b70dfe413607
Jonathan Wylie <jonathan.wylie@gmail.com>
2013-07-22 14:19:30 +0100

    a little tidy

--- sleuth/__init__.py
+++ sleuth/__init__.py
@@ -25,35 +25,19 @@ class Sleuth_Web_App(object):
 
 class Story(object):
 	"""The class represents a Pivotal Tracker User Story"""
-  <id type="integer">387555039</id>
-  <version type="integer">8659</version>
-  <event_type>story_create</event_type>
-  <occurred_at type="datetime">2013/07/15 19:58:39 UTC</occurred_at>
-  <author>Jonathan Wylie</author>
-  <project_id type="integer">138737</project_id>
-  <description>Jonathan Wylie added &quot;test - new story&quot;</description>
-  <stories type="array">
-    <story>
-      <id type="integer">53421649</id>
-      <url>https://www.pivotaltracker.com/services/v3/projects/138737/stories/53421649</url>
-      <name>test - new story</name>
-      <story_type>bug</story_type>
-      <description>the description</description>
-      <current_state>unscheduled</current_state>
-      <requested_by>Jonathan Wylie</requested_by>
 
 	@static
 	def create(story, activity):
 		return Story(story.id, activity.project_id,
-			         story.story_type, story.url, _story.estimate, story.current_state,
-			         story.description, story.name, story.requested_by, _story.owned_by,
-			         _story.created_at, _story.accepted_at, _story.labels)
+			           story.story_type, story.url, _story.estimate, story.current_state,
+			           story.description, story.name, story.requested_by, _story.owned_by,
+			           create_at=_story.created_at, accepted_at=_story.accepted_at, labels=_story.labels)
 
 
 	def __init__(self, story_id, project_id,
 	             story_type, url, estimate, current_state,
 	             description, name, requested_by, owned_by,
-	             created_at, accepted_at, labels):
+	             created_at=None, accepted_at=None, labels=[]):
 		self.id = story_id
 		self.project_id = project_id
 		self.story_type = story_type
